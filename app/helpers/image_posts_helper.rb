83e7cba9b4c2f43d2048a6bf438ece480f89fa59
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-29 15:17:50 +0000

    Started image comments

--- app/helpers/image_posts_helper.rb
+++ app/helpers/image_posts_helper.rb
@@ -2,6 +2,10 @@
 
 # Helper functions for the ImagePosts controller
 module ImagePostsHelper
+  def find_image_post_email(post)
+    User.find(post.user_id).email
+  end
+
   def get_images_by_user(user_id)
     ImagePost.where(user_id: user_id)
   end
