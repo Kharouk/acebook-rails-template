d4fddd1c8199f9e4467485d7febff419e1ff94a1
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-01 09:25:40 +0000

    Fixed test coverage

---
+++ spec/helpers/profiles_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# Tests for the Profiles helper
+RSpec.describe ProfilesHelper, type: :helper do
+  describe '#comments_posts' do
+    it 'gets the profile image comments & posts' do
+      
+    end
+  end
+end
